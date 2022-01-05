de81eaaf0b1849be7617ea02c15aca03f82ba68d
farzanapomy <farzanapomy56@gmail.com>
2022-01-05 19:39:35 +0600

    game over

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 
 
-let count = 0;
+let count = 1;
 function allBox(clickBox) {
-    if (count % 2 != 1) {
-        document.getElementById(clickBox.id).innerHTML = "X"
+    if (count <= 9) {
+
+        if (count % 2 != 0) {
+            document.getElementById(clickBox.id).innerHTML = "X"
+        }
+        else {
+            document.getElementById(clickBox.id).innerHTML = "O"
+        }
+        count++;
+
+        if (winner()) {
+            alert("winner")
+            // document.getElementById("alert").innerHTML = "Winner"
+        }
     }
     else {
-        document.getElementById(clickBox.id).innerHTML = "O"
+        document.getElementById("alert").innerHTML = "Match Draw"
+
     }
-    count++
 }
 
+
+
+
+
+function winner() {
+    if (shortBox("box-1", "box-2", "box-3") || shortBox("box-4", "box-5", "box-6") || shortBox("box-7", "box-8", "box-9") || shortBox("box-1", "box-5", "box-9") || shortBox("box-1", "box-4", "box-7") || shortBox("box-2", "box-5", "box-8") || shortBox("box-3", "box-6", "box-9")) {
+
+        return true;
+    }
+}
+
+function shortBox(box1, box2, box3) {
+    matching(box1) != "" && matching(box2) != "" &&
+        matching(box3) != "" &&
+        matching(box1) ==
+        matching(box2) &&
+        matching(box2) ==
+        matching(box3)
+}
+
+
+function matching(box) {
+    return document.getElementById(box).innerHTML;
+
+}
